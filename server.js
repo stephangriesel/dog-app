a8cb79ebac83506c9c0c99a15ac8d07bd54d4140
Stephan Griesel <sgriesel@gmail.com>
2018-11-28 10:29:30 +0100

    updated - bug: ELIFECYCLE

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const schema = require('./schema');
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-  schema,
+  schema: schema,
   graphiql: true
 }));
 
-const PORT = process.env.PORT || 5000
+app.listen(4000);
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+//const PORT = process.env.PORT || 5000
+
+//app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
