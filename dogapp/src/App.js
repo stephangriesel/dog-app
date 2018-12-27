6a97a741bd63cec775e9a15faca37bf1e05527b7
Stephan Griesel <sgriesel@gmail.com>
2018-12-27 16:22:58 +0100

    pull data from api

--- dogapp/src/App.js
+++ dogapp/src/App.js
@@ -1,27 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://api.thedogapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=4')
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        isLoaded: true,
+        items: json,
+      })
+    });
+
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+    var { isLoaded, items } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>
+    } else {
+
+      return (
+        <div className="App">
+          <ul>
+            {items.map(item => (
+              <li key={item.id}>
+              {/*{console.log(items)*/}
+              {console.log(items[0])}
+              {console.log(items[0].breeds[0].bred_for)}
+              Name: {items[1].breeds[0].name} | Bred for: {items[1].breeds[0].bred_for}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )
+    };
+    
   }
 }
 
